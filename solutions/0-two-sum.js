79dd17de014141cdb7496375a4a711bd25d4d63d
Hossein Talebi <hossein.w7979@gmail.com>
2026-06-29 20:02:59 +0330

    feat: solve two sum problem

---
+++ solutions/0-two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+
+var twoSum = function (nums, target) {
+    let indices = [];
+
+    nums.forEach((currentNum, currentIndex) => {
+        nums.slice(currentIndex + 1).forEach((nextNum, offset) => {
+            if (currentNum + nextNum === target) {
+                indices = [currentIndex, currentIndex + offset + 1];
+            }
+        });
+    });
+
+    return indices;
+};
