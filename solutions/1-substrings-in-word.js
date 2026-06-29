aa765d5f532ba28232c5ed8e4f04a863427c3946
Hossein Talebi <hossein.w7979@gmail.com>
2026-06-29 20:03:30 +0330

    feat: solve substrings in word problem

---
+++ solutions/1-substrings-in-word.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+
+var numOfStrings = function(patterns, word) {
+    
+    let finalNumber = 0
+    patterns.forEach(string => {
+        if(word.includes(string)) [
+          finalNumber =  finalNumber + 1
+        ]
+    });
+
+    return finalNumber
+};
+
+console.log("numOfStrings",numOfStrings(["abcd","abc","bc","d"],"abc"))
